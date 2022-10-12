c6a11f13d03e4b2b61cd3f886f55fd786bddd379
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-10-12 22:09:38 +0900

    Add: validation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }
+
   has_many :tasks
   has_many :favorite, dependent: :destroy
   has_many :favorite_tasks, through: :favorites, source: :task
 
   has_secure_password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    length: { maximum: 254 }
   validates :birthday, presence: true
   validates :gender, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   enum gender: { male: 0, female: 1 }
 end
