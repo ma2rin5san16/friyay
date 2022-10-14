e3d2be96b8eadbcaf520202dbf696b93476c9651
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-10-14 10:30:24 +0900

    Add: レイアウト編集、Add: ログイン機能にコメント追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,23 @@
 module SessionsHelper
 
+  #ログイン処理のメソッド。user.idをsession[:user_id]として暗号化してブラウザに保存
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  #session[:user_id]に値が入っている時、current_userはsession[:user_id]を持つものをログイン中のユーザーとする。
   def current_user
     if session[:user_id]
     @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
+ # ログインしているユーザーであればtrueを返す・
   def current_user?
     user == current_user
   end
 
+  #ログインしているユーザーがいればtrue、ログインしていなければfalseを返す。
   def logged_in?
     !current_user.nil?
   end
