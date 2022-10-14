950b1a6fb2e6b5e9548ea3f09d5a5bdf26703d2e
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-10-14 16:39:07 +0900

    Add: 金曜日までのカウントダウンを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  require 'Date'
+
+  def countdown_to_friday
+    if Date.today.wday == 5
+      "本日は金曜日です"
+    elsif Date.today.wday == 4
+      "金曜日まであと1日"
+    elsif Date.today.wday == 3
+        "金曜日まであと2日"
+    elsif Date.today.wday == 2
+        "金曜日まであと3日"
+    elsif Date.today.wday == 1
+        "金曜日まであと4日"
+    elsif Date.today.wday == 0
+        "金曜日まであと5日"
+    else Date.today.wday == 6
+        "金曜日まであと6日"
+    end
+  end
+
 end
