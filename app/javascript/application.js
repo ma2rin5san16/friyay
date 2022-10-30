431db3d6ef56b630066ddc7adabaec2af0e9e215
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-10-30 11:19:48 +0900

    Fix: 星マーク表示の修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require custom/jquery.raty
 //= require jquery3
 //= require jquery_ujs
 //= require jquery
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
-import './custom/jquery.raty';
 import "@hotwired/turbo-rails"
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
