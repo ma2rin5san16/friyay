b36421f6d49f88876dee23ad1fb68de851d31aa5
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-11-14 11:00:41 +0900

    Add: プロフィールコントローラーの設置

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+  before_action :set_user
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if user.update(user_params)
+      redirect_to profile_path, success: "登録内容を編集しました"
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :birthday, :gender, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(current_user.id)
+    end
+end
