e94238ea49b90c5e1fee04ee4b3cbf77a3af14c8
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-11-21 13:21:52 +0900

    Fix: レイアウト修正

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -10,22 +10,18 @@ class RankingsController < ApplicationController
     @range = params[:range]
     @gender = params[:gender]
 
-    if @range == "1" || @gender == "1"
+    if @range == "1" && @gender == "1"
       @tasks = Task.find(Favorite.with_user.search_with_gender(0).group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
-    elsif @range == "1" || @gender == "2"
-    @tasks = Task.find(Favorite.with_user.search_with_gender(1).group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
-    elsif @range == "2" || @gender == "1"
+    elsif @range == "1" && @gender == "2"
+      @tasks = Task.find(Favorite.with_user.search_with_gender(1).group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id)) 
+    elsif @range == "2" && @gender == "1"
       @tasks = Task.find(Favorite.with_user.search_with_gender(0).search_with_weekly.group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
-    elsif @range == "2" || @gender == "2"
+    elsif @range == "2" && @gender == "2"
       @tasks = Task.find(Favorite.with_user.search_with_gender(1).search_with_weekly.group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
     elsif @range == "1"
       @tasks = Task.find(Favorite.group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
     elsif @range == "2"
-      @tasks = Task.find(Favorite.group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
-    elsif @gender == "1"
-      @tasks = Task.find(Favorite.with_user.search_with_gender(0).group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
-    elsif @gender == "2"
-      @tasks = Task.find(Favorite.with_user.search_with_gender(1).group_by_task.order('count(task_id) desc').limit(3).pluck(:task_id))
+      @tasks = Task.find(Favorite.group_by_task.search_with_weekly.order('count(task_id) desc').limit(3).pluck(:task_id))
     end
 
     @favorite = Favorite.find_by(user_id: current_user.id)
