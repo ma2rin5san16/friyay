6534c6abaa7bdb7ae535675ba98741261726737f
iso <95573057+ma2rin5san16@users.noreply.github.com>
2022-11-27 21:31:44 +0900

    Fix: 修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
+    user_name: Rails.application.credentials.gmail.user_name,
+    password: Rails.application.credentials.gmail.password,
     authentication: :login,
     enable_starttls_auto:  true
   }
